3419a8dc57318ed17f7966053ffc0678af3a17c9
solomon911 <solomonsahayaraj911@gmail.com>
2020-03-18 01:40:53 +0530

    3 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.scss' ]
 })
 export class AppComponent  {
   name = 'Angular';
+  submissions: any;
+  constructor(){
+    this.submissions = [{
+      title: "sample title",
+      url: "sampleuerl.com",
+      permalink: "reddit.com",
+    },
+    {
+      title: "sample title",
+      url: "sampleuerl.com",
+      permalink: "reddit.com",
+    },
+    {
+      title: "sample title",
+      url: "sampleuerl.com",
+      permalink: "reddit.com",
+    },
+    {
+      title: "sample title",
+      url: "sampleuerl.com",
+      permalink: "reddit.com",
+    },
+    {
+      title: "sample title",
+      url: "sampleuerl.com",
+      permalink: "reddit.com",
+    },
+    {
+      title: "sample title",
+      url: "sampleuerl.com",
+      permalink: "reddit.com",
+    }]
+  }
 }
