4ca823439efeff11e6ebc26bad4d15cd85e8b8f1
solomon911 <solomonsahayaraj911@gmail.com>
2020-03-18 14:07:58 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from "rxjs/Observable";
+import * as _ from 'lodash';
+import 'rxjs/add/operator/map'
 
+interface Submission {
+    title: string;
+    url:string;
+    permalink:string;
+}
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.scss' ]
 })
+
 export class AppComponent  {
   name = 'Angular';
   submissions: any;
-  constructor(){
+  constructor(private http:HttpClient){
     this.submissions = [{
       title: "Tom and Rita Hanks have been released from the hospital!",
       url: "https://www.cnn.com/2020/03/16/entertainment/tom-hanks-rita-wilson-released-hospital-coronavirus/index.html",
@@ -40,4 +50,12 @@ export class AppComponent  {
       permalink: "reddit.com",
     }]
   }
+
+  courses$: Observable<Submission[]>;
+
+    ngOnInit() {
+        this.http
+            .get<any>("https://d5emlge6rk.execute-api.us-east-1.amazonaws.com/dev/")
+            .subscribe(responseData => console.log(responseData));
+    }
 }
